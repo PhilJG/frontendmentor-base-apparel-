937f0335dd27136b6d7e9ab942b1286a1640040f
Phil J Gray <phl.j.gray@gmail.com>
2021-09-26 08:08:39 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const input = document.getElementsByClassName('email-input');
+const button = document.getElementsByClassName('btn');
+
+function emailEnter() {
+    if {
+        input.text.
+    }
+}
